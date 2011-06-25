94f8d37db87f163e3e52e610cc60832a309f7043
James Dunn <jamesldunnjr@gmail.com>
2011-06-24 23:47:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rustic::Application.config.secret_token = '7f1fc0dbfa44755cdb053ae4e1747b01ff78342105bfe536cf4cc1367bd7037f24ec675d029863e0c90c5fc8ad6d361e913af7119ed578075c6066dc55729b59'
