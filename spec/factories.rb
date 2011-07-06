c3c44e6b6e7ed9f2fac178b6582378284123811c
James Dunn <jamesldunnjr@gmail.com>
2011-07-06 08:24:32 -0400

    improved the user show page, tested with factory

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+Factory.define :user do |user|
+  user.fname                 "Joseph"
+  user.lname                 "Khamel"
+  user.email                 "joseph.khamel@example.com"
+  user.tel                   "123.456.7890"
+  user.password              "pa55w0rD"
+  user.password_confirmation "pa55w0rD"
+end
