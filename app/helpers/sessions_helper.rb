c09c1f0e38db05239ed6da4200bf6b0f063bcd32
James Dunn <jamesldunnjr@gmail.com>
2011-07-20 10:08:33 -0400

    debugged the friendly forwarding...finally.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,13 +8,17 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
   
-  def signed_in?
-    !current_user.nil?
+  def current_user?(user)
+    user == current_user
   end
   
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     self.current_user = user
+  end
+  
+  def signed_in?
+    !current_user.nil?
   end
   
   def sign_out
@@ -32,14 +36,6 @@ module SessionsHelper
     clear_return_to
   end
   
-  def current_user?(user)
-    user == current_user
-  end
-
-  def deny_access
-    redirect_to signin_path, :notice => "Please sign in to access this page."
-  end
-  
   private
 
     def remember_token
@@ -53,7 +49,7 @@ module SessionsHelper
     def store_location
       session[:return_to] = request.fullpath
     end
-
+    
     def clear_return_to
       session[:return_to] = nil
     end
