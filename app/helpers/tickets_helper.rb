40ecfc2a34c9bd229b1a8acb614f1f8b48760a91
James Dunn <jamesldunnjr@gmail.com>
2011-08-30 15:52:19 -0400

    tickets index slightly less verbose, lest we overflow the container div.

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,8 +1,8 @@
 module TicketsHelper
   def ticket_index_row(ticket)
     ["#{ticket.customer.fname} #{ticket.customer.lname}",
-    ticket.computer.make, ticket.computer.model, truncate(ticket.issue),
-    (ticket.technician) ? "#{ticket.technician.fname} #{ticket.technician.lname.first}." : "None",
+    "#{ticket.computer.make} #{ticket.computer.model}", truncate(ticket.issue, :length => 20),
+    (ticket.technician) ? "#{ticket.technician.fname.first}#{ticket.technician.lname.first}" : "--",
     (ticket.status.nil?) ? "None" : ticket.status,
     "#{time_ago_in_words(ticket.created_at)} ago"]
   end
