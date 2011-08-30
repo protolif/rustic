8036e95bf1a10265ffd6ba052048cb78df3d9d0b
James Dunn <jamesldunnjr@gmail.com>
2011-08-30 15:56:12 -0400

    even less verbose ticket index

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,7 +1,8 @@
 module TicketsHelper
   def ticket_index_row(ticket)
     ["#{ticket.customer.fname} #{ticket.customer.lname}",
-    "#{ticket.computer.make} #{ticket.computer.model}", truncate(ticket.issue, :length => 20),
+    truncate("#{ticket.computer.make} #{ticket.computer.model}", :length => 20),
+    truncate(ticket.issue, :length => 20),
     (ticket.technician) ? "#{ticket.technician.fname.first}#{ticket.technician.lname.first}" : "--",
     (ticket.status.nil?) ? "None" : ticket.status,
     "#{time_ago_in_words(ticket.created_at)} ago"]
