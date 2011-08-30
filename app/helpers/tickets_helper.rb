afb0499334cfb7305a2e7bd4d20f447ddc0bafd2
James Dunn <jamesldunnjr@gmail.com>
2011-08-30 16:04:10 -0400

    removed the word 'ago' from time on tickets index

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -5,6 +5,6 @@ module TicketsHelper
     truncate(ticket.issue, :length => 20),
     (ticket.technician) ? "#{ticket.technician.fname.first}#{ticket.technician.lname.first}" : "--",
     (ticket.status.nil?) ? "None" : ticket.status,
-    "#{time_ago_in_words(ticket.created_at)} ago"]
+    time_ago_in_words(ticket.created_at)]
   end
 end
