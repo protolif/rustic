458f66bb78cec3fae3d848089c863f42ffd61492
James Dunn <jamesldunnjr@gmail.com>
2013-07-16 13:14:51 -0400

    making the computer form work

--- app/assets/javascripts/computers.js.coffee
+++ app/assets/javascripts/computers.js.coffee
@@ -1,5 +1,15 @@
 $ ->
-  $('.submitless input').change ->
+  # Submitless forms: because submit buttons are over.
+  # submit textboxes right away
+  $(".submitless input[type='text']").change ->
     $(this).closest('.submitless').submit()
+  # don't submit checkboxes right away
+  # wait for the slider transition to finish
+  $(".submitless input[type='checkbox']").change ->
+    delay  = 1000# 1 second
+    slider = $(this)
+    submit_the_form = -> slider.closest('.submitless').submit()
+    setTimeout submit_the_form, delay
+  # dropdown box (select) support
   $('.submitless select').change ->
     $(this).closest('.submitless').submit()
--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -8,10 +8,21 @@ class ComputersController < ApplicationController
   end
 
   def update
-    # @computer = Computer.find params[:id]
-    # if @computer.update_attributes(params[:computer])
-    #   redirect_to @computer
-    # end
-    raise params.to_yaml
+    @computer = Computer.find params[:id]
+    if @computer
+      if @computer.update_attributes!(computer_params)
+        redirect_to @computer
+      else
+        raise 'herp'.inspect
+      end
+    else
+      raise 'derp'.inspect
+    end
   end
+
+  private
+    def computer_params
+      params.require(:computer).permit(:make, :model, :serial, :form_factor, :charger, :location)
+    end
+  # end private
 end
