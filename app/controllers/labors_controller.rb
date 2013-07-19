5095d99a2380fb8ef2b369787c67609f4bc680f2
James Dunn <james.dunn@protolif.com>
2013-07-19 14:08:23 -0400

    the ticket labor index is composed of rows of little submitless forms

--- app/controllers/labors_controller.rb
+++ app/controllers/labors_controller.rb
@@ -7,4 +7,23 @@ class LaborsController < ApplicationController
       raise 'ticket not found'.inspect
     end
   end
+
+  def update
+    @labor = Labor.find params[:id]
+    if @labor.present?
+      if @labor.update_attributes(labor_attributes)
+        redirect_to ticket_labors_path(@labor.ticket)
+      else
+        raise 'unable to update labor'.inspect
+      end
+    else
+      raise 'unable to find labor'.inspect
+    end
+  end
+  
+  private
+    def labor_attributes
+      params.require(:labor).permit(:service, :notes, :price)
+    end
+  #end private
 end
