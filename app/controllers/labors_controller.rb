e768cb2c3f48cf6b4e6d9111df413ef37bd6b7a5
James Dunn <james.dunn@protolif.com>
2013-07-24 22:48:11 -0400

    new labor controller and view working. just needs style

--- app/controllers/labors_controller.rb
+++ app/controllers/labors_controller.rb
@@ -20,6 +20,23 @@ class LaborsController < ApplicationController
       raise 'unable to find labor'.inspect
     end
   end
+
+  def new
+    @ticket = Ticket.find params[:ticket_id]
+    @labor = @ticket.labors.new
+  end
+
+  def create
+    if @ticket = Ticket.find(params[:ticket_id])
+      if @ticket.labors.create!(labor_attributes)
+        redirect_to ticket_labors_path(@ticket)
+      else
+        render :new
+      end
+    else
+      raise 'unable to find ticket'.inspect
+    end
+  end
   
   private
     def labor_attributes
